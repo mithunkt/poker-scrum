d9420487209ee714a78ea9a1f3e3ea7d4cf11bb1
mithunkt <mithun.thachareyath@akqa.com>
2020-02-28 10:10:50 +1100

    Updates

---
+++ precache-manifest.1f58a822342adb9bb7eb50d6595caa2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f86e40a668d4501ab0fedc9d1a86cc",
+    "url": "/poker-scrum/index.html"
+  },
+  {
+    "revision": "a8c1828ecbc049c6642c",
+    "url": "/poker-scrum/static/css/main.f49af803.chunk.css"
+  },
+  {
+    "revision": "70a7cf173d62d06d97d5",
+    "url": "/poker-scrum/static/js/2.f76f8b07.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/poker-scrum/static/js/2.f76f8b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c1828ecbc049c6642c",
+    "url": "/poker-scrum/static/js/main.cc1d7f8c.chunk.js"
+  },
+  {
+    "revision": "a3bffb0e43b583eecf08",
+    "url": "/poker-scrum/static/js/runtime-main.a93d6388.js"
+  }
+]);
